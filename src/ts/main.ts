17145e5d1c1fc69270e13fca777161fbcf7cac53
Andressa Souza <afariasouza@outlook.com>
2025-02-11 15:43:30 -0300

    Mudanças no Modal

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,6 @@
+var myModal = document.getElementById('myModal') as HTMLElement;
+var myInput = document.getElementById('myInput') as HTMLElement;
 
+myModal.addEventListener('shown.bs.modal', function () {
+	myInput.focus();
+});
